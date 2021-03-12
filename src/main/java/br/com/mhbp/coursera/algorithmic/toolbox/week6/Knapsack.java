69861b49dc5f6e69a91cc43a6879f504ea6f5ae5
marcos <marcoscba@gmail.com>
2021-03-12 15:34:09 -0300

    knapsack without repetition

--- src/main/java/br/com/mhbp/coursera/algorithmic/toolbox/week6/Knapsack.java
+++ src/main/java/br/com/mhbp/coursera/algorithmic/toolbox/week6/Knapsack.java
@@ -16,49 +16,60 @@ Correct output:
 19
 
 
+10 5
+3 5 3 3 5
+
+10
+
+
+
+Input:
+20 4
+5 7 12 18
+
+Your output:
+17
+
+Correct output:
+19
+
+
+
+
     * */
     static int optimalWeight(int W, int[] items) {
 
-        Arrays.sort(items);
-        int[][] dp = new int [items.length] [W + 1] ;
+//        Arrays.sort(items);
+
+        int[][] dp = new int [items.length + 1] [W + 1] ;
 
         //if we only one bar we tale since the capacity 'W' still greater or equal than the weight's bar
-        for (int i = 1; i <= W; i++) {
-            if (items[0] <= W)
-                dp[0][i] = items[0];
-        }
+//        for (int i = 1; i <= W; i++) {
+//            if (items[0] <= W)
+//                dp[0][i] = items[0];
+//        }
 
 //        System.out.println("start processing");
 //        print(dp);
-        for (int i = 1; i < items.length; i++) {
-            int currentBar =  items[i];
-            for (int w = 1  ; w <= W; w++) {
+        for (int i = 1; i <= items.length; i++) {
 
-//5 7 12 18
-                if (currentBar > w) {
-                    dp[i][w] = dp[i -1][w];
-                } else {
-//                    System.out.println(" including bar:" + (dp[i-1][w - currentBar] + currentBar));
-//
-                        int profit1 = dp[i-1][w - currentBar] + currentBar;
-                        if (profit1 > W)
-                            profit1 = dp[i][w - 1];
-                        int profit2 = dp[i-1][w];
-                        dp[i][w] = Math.max(profit1, profit2);
+            for (int w = 1; w <= W ; w++) {
+
+                dp[i][w] = dp[i - 1][w];
+
+                if (items[i -1] <= w) {
+
+                    int current = dp[i - 1][w - items[i -1]] + items[i -1];
+                    if (current > dp[i][w]) {
+                        dp[i][w] = current;
+                    }
                 }
+
             }
-
-//            System.out.println();
-//            System.out.println();
-//            printBars(items);
-//            System.out.println(" solution including up to the item " + i);
-//            print(dp);
         }
-
-        System.out.println("finish processing");
 //        print(dp);
-
-        return dp[items.length -1][W];
+//        System.out.println();
+        return dp[items.length][W];//check
     }
 
     static void printBars(int[] bars) {
